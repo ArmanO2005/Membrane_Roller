895e1f541383328279c21efe1f44caa07fae4ee9
ArmanO2005 <cyclanthaceae@berkeley.edu>
2026-06-15 14:55:50 -0700

    stake configurable

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-from c_motor_control.Clamp_utils import ClampController
-from c_motor_control.Staker_utils import StakerController
-from c_motor_control.Feeder_utils import FeederController
-from c_motor_control.Spindle_utils import SpindleController
-import time
-import yaml
-import logging
+# from c_motor_control.Clamp_utils import ClampController
+# from c_motor_control.Staker_utils import StakerController
+# from c_motor_control.Feeder_utils import FeederController
+# from c_motor_control.Spindle_utils import SpindleController
+# import time
+# import yaml
+# import logging
 
-with open('a_config/motor_config.yaml', 'r') as file:
-    motor_config = yaml.safe_load(file)
-
-
-clamp_motor = ClampController(motor_config['clamp'])
-staker_motor = StakerController(motor_config['staker'])
-spindle_motor = SpindleController(motor_config['spindle'])
-feeder_motor = FeederController(motor_config['feeder'])
-
-staker_motor.stake()
+# with open('a_config/motor_config.yaml', 'r') as file:
+#     motor_config = yaml.safe_load(file)
 
 
-# from gui import main
+# clamp_motor = ClampController(motor_config['clamp'])
+# staker_motor = StakerController(motor_config['staker'])
+# spindle_motor = SpindleController(motor_config['spindle'])
+# feeder_motor = FeederController(motor_config['feeder'])
 
-# main()
+# staker_motor.stake()
+
+
+from gui import main
+
+main()
