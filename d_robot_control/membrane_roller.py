4bd9287e6b05199cd60839dd9f37a5544821fcf0
Arman Omidvar <cyclanthaceae@berkeley.edu>
2026-06-25 13:23:27 -0700

    tensionless rotation

--- d_robot_control/membrane_roller.py
+++ d_robot_control/membrane_roller.py
@@ -39,7 +39,7 @@ class MembraneRoller:
         while True:
             not_triggered = self.spindle_motor.motor.get_axis_parameter(self.spindle_motor.AP.HomeSwitch)
             if not not_triggered:
-                time.sleep(0.7)
+                time.sleep(self.spindle_motor.config.get("velocity") / 1714)
                 self.spindle_motor.motor.stop()
                 self.feeder_motor.stop()
                 print(f"[{self.spindle_motor.name}] Switch triggered.")
