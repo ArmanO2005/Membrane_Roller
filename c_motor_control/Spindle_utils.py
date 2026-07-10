1866f58393eedaf878d26e01443878b1aad3f1fc
Arman Omidvar <cyclanthaceae@berkeley.edu>
2026-07-10 10:35:03 -0700

    move to test spindle

--- c_motor_control/Spindle_utils.py
+++ c_motor_control/Spindle_utils.py
@@ -15,26 +15,27 @@ class SpindleController(TMCM1111Controller):
         super().home()
 
         self.motor.set_axis_parameter(self.AP.ActualPosition, 0)
-        
-        # self.motor.rotate(self.config.get("velocity"))
-        # time.sleep(self.config.get("notch_time"))        
-        # self.motor.stop()
 
-        self.move_to(500, velocity=self.config.get("velocity"))
-
+    def move_to_notch(self):
+        self.move_to(self.config.get("notch_position"), velocity=self.config.get("velocity"))
 
     def full_rotation(self):
-        self.motor.rotate(self.config.get("velocity"))
-        time.sleep(2)
-        start_time = time.time()
-        while True:
-            not_triggered = self.motor.get_axis_parameter(self.AP.HomeSwitch)
-            if not not_triggered:
-                time.sleep(self.config.get("home_centering_time"))
-                time.sleep(self.config.get("notch_time"))
-                self.motor.stop()
-                break
-            if time.time() - start_time > self.config.get("home_timeout"):
-                self.motor.stop()
-                raise TimeoutError(f"[{self.name}] Homing timed out")
-            time.sleep(0.01)
+        current_position = self.motor.get_axis_parameter(self.AP.ActualPosition)
+        self.move_to(current_position, velocity=self.config.get("velocity"))
+
+
+    # def full_rotation(self):
+    #     self.motor.rotate(self.config.get("velocity"))
+    #     time.sleep(2)
+    #     start_time = time.time()
+    #     while True:
+    #         not_triggered = self.motor.get_axis_parameter(self.AP.HomeSwitch)
+    #         if not not_triggered:
+    #             time.sleep(self.config.get("home_centering_time"))
+    #             time.sleep(self.config.get("notch_time"))
+    #             self.motor.stop()
+    #             break
+    #         if time.time() - start_time > self.config.get("home_timeout"):
+    #             self.motor.stop()
+    #             raise TimeoutError(f"[{self.name}] Homing timed out")
+    #         time.sleep(0.01)
