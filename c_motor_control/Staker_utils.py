08eba21d847b7998a2728e9cb0938f74b5d00399
Arman Omidvar <cyclanthaceae@berkeley.edu>
2026-07-29 12:01:58 -0700

    new staker homing behavior

--- c_motor_control/Staker_utils.py
+++ c_motor_control/Staker_utils.py
@@ -20,6 +20,39 @@ class StakerController(TMCM1111Controller):
     def go_to_0(self):
         self.move_to(1000, velocity=self.config.get("home_search_velocity"))
 
+    # def home(self):
+    #     print(f"[{self.name}] Homing started...")
+
+    #     if not self.motor.get_axis_parameter(self.AP.HomeSwitch):
+    #         self.motor.rotate(self.config.get("home_search_velocity"))
+    #         time.sleep(2)
+    #         self.motor.stop()
+    #         self.wait_for_stop()
+
+    #     self.motor.rotate(-self.config.get("home_search_velocity"))
+    #     start_time = time.time()
+    #     while True:
+    #         not_triggered = self.motor.get_axis_parameter(self.AP.HomeSwitch) # 0: triggered, 1: not_triggered
+    #         if not not_triggered:
+    #             self.motor.stop()
+    #             self.wait_for_stop()
+    #             centering_time = self.config.get("home_centering_time")
+    #             if centering_time:
+    #                 self.motor.rotate(self.config.get("home_search_velocity"))
+    #                 time.sleep(centering_time)
+    #                 self.motor.stop()
+    #                 self.wait_for_stop()
+    #             print(f"[{self.name}] Switch triggered.")
+    #             break
+    #         if time.time() - start_time > self.config.get("home_timeout"):
+    #             self.motor.stop()
+    #             raise TimeoutError(f"[{self.name}] Homing timed out")
+    #         time.sleep(0.01)
+
+    #     self.motor.set_axis_parameter(self.AP.ActualPosition, 0)
+    #     print(f"[{self.name}] Homing complete. Position zeroed.")
+
+
     def home(self):
         print(f"[{self.name}] Homing started...")
 
@@ -38,7 +71,7 @@ class StakerController(TMCM1111Controller):
                 self.wait_for_stop()
                 centering_time = self.config.get("home_centering_time")
                 if centering_time:
-                    self.motor.rotate(self.config.get("home_search_velocity"))
+                    self.motor.rotate(-self.config.get("home_search_velocity"))
                     time.sleep(centering_time)
                     self.motor.stop()
                     self.wait_for_stop()
